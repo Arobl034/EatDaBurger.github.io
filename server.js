cf0a89c9e428a1a35684b8353c1dfaa7224c486a
Arobl034 <anthonyrobles@Anthonys-iMac.attlocal.net>
2020-04-03 17:35:06 -0400

    app models/routes

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
+// Dependencies
+// =============================================================
 var express = require("express");
-
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Set Handlebars.
+var bodyParser = require("body-parser");
+var handlebars = require("handlebars");
 var exphbs = require("express-handlebars");
 
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+app.use(express.static(__dirname + "/public"));
 
-app.use(routes);
+// Requiring our models for syncing
+var db = require("./models");
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Routes 
+// ============================================================
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
